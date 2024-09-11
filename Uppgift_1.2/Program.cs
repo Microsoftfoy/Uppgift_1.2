e3a626a805056fbd4ecc149b7286fdd6ee4b7077
ercr07001 <ercr07001@E-PW05CGR0>
2024-09-11 12:36:45 +0200

    Add project files.

---
+++ Uppgift_1.2/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace TEST
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Blue;
+            Console.Clear();
+            Console.WriteLine("Error 404 Not Found.");
+            Console.WriteLine("Computer has corrupted");
+            Console.WriteLine(":(");
+            Console.WriteLine("");
+            Console.WriteLine("Restart the computer");
+            Console.WriteLine("Or");
+            Console.WriteLine("Delete everything");
+
+        }
+    }
+}
+
